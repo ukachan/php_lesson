e94b7140d15d66b97cf7f5e15d08437f324319d6
tsuyoshi komuro <komuro.0418@gmail.com>
2018-05-12 14:29:59 +0900

    unset.php add

--- session/nextpage.php
+++ session/nextpage.php
@@ -2,5 +2,17 @@
 session_start();
 $userid = $_SESSION['userid'];
 $username = $_SESSION['username'];
+$cookie = $_COOKIE['PHPSESSID'];
 
-echo($userid . $username);
+echo($userid . $username . '/' . $cookie);
+?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>nextpage</title>
+    </head>
+    <body>
+        <p>リセットするには<a href="./unset.php">ここをクリック</a>
+    </body>
+</html>
